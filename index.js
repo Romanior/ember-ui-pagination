eee7aa0fb9d93f838d338da72f8187c34595de61
Romanior <roma.khudyakov@gmail.com>
2015-10-12 22:39:33 +0200

    first commit

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ui-pagination'
+  name: 'ember-ui-pagination',
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+  included: function(app) {
+    this._super.included(app);
+    this.app.import(app.bowerDirectory + '/comma-separated-values/csv.js');
+  }
 };
