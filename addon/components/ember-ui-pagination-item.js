d29849cf656e33f098defb2929b5df6b7bfd2bf1
Romanior <roma.khudyakov@gmail.com>
2015-10-22 21:53:38 +0200

    try to use node 12 for heroku

--- addon/components/ember-ui-pagination-item.js
+++ addon/components/ember-ui-pagination-item.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
+/**
+ *  An actual item pagination iterate over
+ *  You can overwrite it in order to provide different layout
+ *  and more user-specific actions
+ *
+ */
+
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['ember-ui-pagination__item']
